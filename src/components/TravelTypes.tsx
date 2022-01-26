0cdccfb850cc7f2d4e7018640dd09ff02b983f6e
ThiagoRovetta <rovetta.thiago@gmail.com>
2022-01-26 08:36:23 -0300

    Created TravelTypes component.

---
+++ src/components/TravelTypes.tsx
@@ -0,0 +1,140 @@
+import { Flex, Image, Text } from "@chakra-ui/react";
+
+export function TravelTypes() {
+  return (
+    <Flex
+      as="div"
+      w="1160px"
+      h="145px"
+      mt="80.79px"
+      mr="140px"
+      ml="140px"
+    >
+      
+      <Flex
+        as="div"
+        direction="column"
+        alignItems="center"
+        mr="130px"
+      >
+        <Image
+          alt="cocktail" 
+          src='cocktail.png' 
+          w="85px" 
+          h="85px" 
+        />
+        <Text
+          color="gray.900" 
+          fontStyle="normal"
+          fontWeight="600"
+          fontSize="24px"
+          lineHeight="36px"
+          align="center"
+          mt="24px"
+        >
+          vida noturna
+        </Text>
+      </Flex>
+
+      <Flex
+        as="div"
+        direction="column"
+        alignItems="center"
+        mr="151px"
+      >
+        <Image
+          alt="surf" 
+          src='surf.png' 
+          w="85px" 
+          h="85px" 
+        />
+        <Text
+          color="gray.900" 
+          fontStyle="normal"
+          fontWeight="600"
+          fontSize="24px"
+          lineHeight="36px"
+          align="center"
+          mt="24px"
+        >
+          praia
+        </Text>
+      </Flex>
+
+      <Flex
+        as="div"
+        direction="column"
+        alignItems="center"
+        mr="171px"
+      >
+        <Image
+          alt="building" 
+          src='building.png' 
+          w="85px" 
+          h="85px" 
+        />
+        <Text
+          color="gray.900" 
+          fontStyle="normal"
+          fontWeight="600"
+          fontSize="24px"
+          lineHeight="36px"
+          align="center"
+          mt="24px"
+        >
+          moderno
+        </Text>
+      </Flex>
+
+      <Flex
+        as="div"
+        direction="column"
+        alignItems="center"
+        mr="151px"
+      >
+        <Image
+          alt="museum" 
+          src='museum.png' 
+          w="85px" 
+          h="85px" 
+        />
+        <Text
+          color="gray.900" 
+          fontStyle="normal"
+          fontWeight="600"
+          fontSize="24px"
+          lineHeight="36px"
+          align="center"
+          mt="24px"
+        >
+          clássico
+        </Text>
+      </Flex>
+
+      <Flex
+        as="div"
+        direction="column"
+        alignItems="center"
+      >
+        <Image
+          alt="earth" 
+          src='earth.png' 
+          w="85px" 
+          h="85px" 
+        />
+        <Text
+          color="gray.900" 
+          fontStyle="normal"
+          fontWeight="600"
+          fontSize="24px"
+          lineHeight="36px"
+          align="center"
+          mt="24px"
+        >
+          e mais...
+        </Text>
+      </Flex>
+
+    </Flex>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,30 @@
-import { Flex } from '@chakra-ui/react'
-import { Banner } from '../components/Banner'
+import { Flex, Divider } from '@chakra-ui/react'
 import { Header } from '../components/Header'
+import { Banner } from '../components/Banner'
+import { TravelTypes } from '../components/TravelTypes'
 
 export default function Home() {
   return (
     <>
       <Flex 
         w={{lg: "1440px", sm: "375px"}} 
-        h="100vh" 
+        h="1469px" 
         direction="column"
         align="center" 
         bg="gray.100"
+        mx="auto"
       >
         <Header isHome />  
         <Banner />  
+        <TravelTypes />  
+        <Flex
+          mt="80px"
+          mb="52px"
+          w="90px"
+          mx="auto"
+        >
+          <Divider color="gray.900" borderBottom="2px" />
+        </Flex>
       </Flex>
     </>
   )
